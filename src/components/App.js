528652371451b509e72c60666d74cbbd453d651d
maurya2711 <sharadmaurya4600@gmail.com>
2021-06-21 14:44:40 +0530

    try

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+
+const App = () => {
+  return (
+    <div id="main">
+      <h3 data-ns-test="project-name">Dream 11 team predictor</h3>
+      <p data-ns-test="project-description">
+        It provides a fantasy gaming platform for multiple sports such as
+        cricket, football, basketball, kabaddi, hockey, volleyball, handball and
+        baseball. It is an online game where users create a virtual team of
+        real-life players and earn points based on the performances of these
+        players in real matches.
+      </p>
+    </div>
+  );
+};
+
+export default App;
